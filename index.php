82339dae6357292df68ce39f91c750cbe20fa34d
Tchissambou Jérémy <jeremy.tchissambou@datategy.net>
2018-12-05 01:16:00 +0100

    Reorganisation dossier + nouvelle page d'accueil

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="CSS/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+
+    <title>Boulangerie Bassignani</title>
+</head>
+<body>
+    <div class="container-fluid h-100">
+        <div class="row front justify-content-center h-100">
+            <div class="col-md-4">
+<br>
+            <article>
+                
+                <h1>Boulangerie</h1>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit et alias, vel aliquid a odit, ullam maiores ratione repellendus obcaecati eligendi odio quia ipsam. Delectus blanditiis nisi quidem aperiam sequi!</p>
+            </article>
+
+            </div>
+            <div class="col-md-4">
+<br>
+                <article>
+                <h1>Viennoiserie</h1>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda repellat vero nihil harum magni provident placeat, dicta ipsam iste quo molestiae dolor quaerat unde, id, impedit sed? Mollitia, nostrum enim!</p>
+            </article>
+            </div>
+            <div class="col-md-4">
+<br>
+                <article>
+                <h1>Patisserie</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum maiores cum autem minima quos aut, voluptatem optio quasi harum accusantium aliquam natus, debitis sint, omnis nam vero! Ad, harum quibusdam.</p>
+            </article>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
