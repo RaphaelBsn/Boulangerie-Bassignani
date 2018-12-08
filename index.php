680b3bce5792ae4ec7c48b8341a0722991d36e1d
Tchissambou Jérémy <jeremy.tchissambou@datategy.net>
2018-12-08 14:24:54 +0100

    Finalisation page portail

--- index.php
+++ index.php
@@ -6,33 +6,43 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="CSS/bootstrap.min.css">
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="CSS/animate.css">
+
 
     <title>Boulangerie Bassignani</title>
 </head>
 <body>
     <div class="container-fluid h-100">
-        <div class="row front justify-content-center h-100">
-            <div class="col-md-4">
+        <div class="row justify-content-center">
+            <div class="col-md-12 enseigne">
+                <h1>Boulangerie Bassignani</h1>
+            </div>
+        </div>
+        <div class="row front justify-content-center">
+            <div class="col-md-4 boulangerie_slot rotateInUpLeft animated">
 <br>
             <article>
                 
-                <h1>Boulangerie</h1>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit et alias, vel aliquid a odit, ullam maiores ratione repellendus obcaecati eligendi odio quia ipsam. Delectus blanditiis nisi quidem aperiam sequi!</p>
+                <h1>Boulangerie</h1> <br> <br>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit et alias, vel aliquid a odit, ullam maiores ratione repellendus obcaecati eligendi odio quia ipsam. Delectus blanditiis nisi quidem aperiam sequi! lorem</p>
+                <a href="#"> > Plus de détails. < </a>
             </article>
 
             </div>
-            <div class="col-md-4">
+            <div class="col-md-4 vieinoiserie_slot rotateIn animated">
 <br>
                 <article>
-                <h1>Viennoiserie</h1>
+                <h1>Viennoiserie</h1><br><br>
                 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda repellat vero nihil harum magni provident placeat, dicta ipsam iste quo molestiae dolor quaerat unde, id, impedit sed? Mollitia, nostrum enim!</p>
+                <a href="#"> > Plus de détails. < </a>
             </article>
             </div>
-            <div class="col-md-4">
+            <div class="col-md-4 patisserie_slot rotateInUpRight animated">
 <br>
                 <article>
-                <h1>Patisserie</h1>
+                <h1>Patisserie</h1> <br><br>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum maiores cum autem minima quos aut, voluptatem optio quasi harum accusantium aliquam natus, debitis sint, omnis nam vero! Ad, harum quibusdam.</p>
+                <a href="#"> > Plus de détails. < </a>
             </article>
             </div>
         </div>
