bb4650de25fa58ea122c9dff5e8cd370bd59a7f0
Raphael <raphael.bassignani91@gmail.com>
2018-12-10 22:18:22 +0100

    maj

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="CSS/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="CSS/animate.css">
+
+
+    <title>Boulangerie Bassignani</title>
+</head>
+<body>
+    <div class="container-fluid h-100">
+        <div class="row justify-content-center">
+            <div class="col-md-12 enseigne">
+                <h1>Boulangerie Bassignani</h1>
+            </div>
+        </div>
+        <div class="row front justify-content-center">
+            <div class="col-md-4 boulangerie_slot rotateInUpLeft animated">
+<br>
+            <article>
+                
+                <h1>Boulangerie</h1> <hr>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit et alias, vel aliquid a odit, ullam maiores ratione repellendus obcaecati eligendi odio quia ipsam. Delectus blanditiis nisi quidem aperiam sequi! lorem</p>
+                <a href="PHP/index_boulangerie.php"> > Plus de détails. < </a>
+            </article>
+
+            </div>
+            <div class="col-md-4 vieinoiserie_slot rotateIn animated">
+<br>
+                <article>
+                <h1>Viennoiserie</h1> <hr>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda repellat vero nihil harum magni provident placeat, dicta ipsam iste quo molestiae dolor quaerat unde, id, impedit sed? Mollitia, nostrum enim!</p>
+                <a href="PHP/index_vieinoiserie.php"> > Plus de détails. < </a>
+            </article>
+            </div>
+            <div class="col-md-4 patisserie_slot rotateInUpRight animated">
+<br>
+                <article>
+                <h1>Patisserie</h1> <hr>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum maiores cum autem minima quos aut, voluptatem optio quasi harum accusantium aliquam natus, debitis sint, omnis nam vero! Ad, harum quibusdam.</p>
+                <a href="PHP/index_patisserie.php"> > Plus de détails. < </a>
+            </article>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
